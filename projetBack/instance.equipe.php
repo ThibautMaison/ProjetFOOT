314605ce4318ae94959230aed215091d8d243965
florentin harmand <florentin.harmand@hotmail.com>
2022-12-05 13:23:48 +0100

    actualisation back equipes

--- projetBack/instance.equipe.php
+++ projetBack/instance.equipe.php
@@ -6,3 +6,4 @@ $allemagne = new Equipe("Allemagne", $allemagnes, "Joachim Low");
 $argentine = new Equipe("Argentine", $argentines, "Jeff Bezos");
 $bresil = new Equipe("Brésil", $bresils, "Pelé");
 $coreeS = new Equipe("Corée du Sud", $coreeSs, "Pape San");
+$equipes = [$allemagne, $argentine, $bresil, $coreeS];
